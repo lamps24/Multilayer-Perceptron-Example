8056b6bd69045dca2db2fd84f5152b2f8ac93382
Alex Lampert <55845056+lamps24@users.noreply.github.com>
2020-03-30 13:11:20 -0500

    Update main_MLP.m

--- main_MLP.m
+++ main_MLP.m
@@ -1,9 +1,7 @@
 %% 4a) 
 
 % read data: call ReadNormalizedOptdigitsDataset
-%%%% YOUR CODE STARTS HERE
 [X_trn_norm, y_trn, X_val_norm, y_val, X_tst_norm, y_tst] = ReadNormalizedOptdigitsDataset("C:\Users\lamps\Documents\Class\CSCI 5521 - Machine Learning\HW3\Question4\optdigits_train.txt", "C:\Users\lamps\Documents\Class\CSCI 5521 - Machine Learning\HW3\Question4\optdigits_valid.txt", "C:\Users\lamps\Documents\Class\CSCI 5521 - Machine Learning\HW3\Question4\optdigits_test.txt");
-%%%%
 
 Hs = [4,8,12,16,20,24];
 training_error = zeros(length(Hs),1);
@@ -14,91 +12,60 @@ for i=1:length(Hs)
     H = Hs(i);
 
     % train MLP using current H using MLPTrain
-    %%%% YOUR CODE STARTS HERE
     [Y_trn_pred,Z,W,V] = MLPTrain(X_trn_norm, y_trn, H);
-    %%%%
 
     % calculate error rate for Y predicted to the training set using CalculateErrorRate
-    %%%% YOUR CODE STARTS HERE
     training_error(i) = CalculateErrorRate(Y_trn_pred, y_trn);
-    %%%%
 
     fprintf('Training set error rate when H=%d: %f\n', H, training_error(i));
     
     % Predict Y for the validation set, using ForwardPropagation
-    %%%% YOUR CODE STARTS HERE
     [Y_val_pred,Z] = ForwardPropagation(X_val_norm, W, V);
-    %%%%
 
     % calculate error rate for Y predicted to the validation set using CalculateErrorRate
-    %%%% YOUR CODE STARTS HERE
     validation_error(i) = CalculateErrorRate(Y_val_pred, y_val);
-    %%%%
     
     fprintf('Validation set error rate when H=%d: %f\n', H, validation_error(i));
     
 end
 
 % Plot training and validation error using PlotTrainingValidationError
-%%%% YOUR CODE STARTS HERE
 PlotTrainingValidationError(Hs,training_error, validation_error);
-%%%% 
-
+ 
 % train MLP using the best number of hidden units MLPTrain
-%%%% YOUR CODE STARTS HERE
 [Y_trn_pred,Z,W,V] = MLPTrain(X_trn_norm, y_trn, 20);
-%%%%
 
 % Predict Y for the test set, using ForwardPropagation
-%%%% YOUR CODE STARTS HERE
 [Y_tst_pred,Z] = ForwardPropagation(X_tst_norm, W, V);
-%%%%
 
 % calculate error rate for Y predicted to the test set using CalculateErrorRate
-%%%% YOUR CODE STARTS HERE
 test_error = CalculateErrorRate(Y_tst_pred, y_tst);
-%%%%
 
 fprintf('Test set error rate when H=%d: %f\n', Hs(5), test_error);
 
-%% 4b) 
-
 % Train the MLP with 2 hidden units, using MLPTrain
-%%%% YOUR CODE STARTS HERE
 [Y_trn_pred,Z_trn,W,V] = MLPTrain(X_trn_norm, y_trn, 2);
-%%%%
 
 % Predict Y for the validation and test set, using ForwardPropagation
-%%%% YOUR CODE STARTS HERE
 [Y_val_pred,Z_val] = ForwardPropagation(X_val_norm, W, V);
 [Y_tst_pred,Z_tst] = ForwardPropagation(X_tst_norm, W, V);
-%%%%
 
 % Do a 2D scatter showing Z for the training, validation and test set in
 % separate figures, using PlotZ2DScatter for each figure
-%%%% YOUR CODE STARTS HERE
 PlotZ2DScatter(Z_trn,Y_trn_pred)
 PlotZ2DScatter(Z_val,Y_val_pred)
 PlotZ2DScatter(Z_tst,Y_tst_pred)
-%%%% 
 
-%% 4c) 
 
 % Train the MLP with 3 hidden units, using MLPTrain
-%%%% YOUR CODE STARTS HERE
 [Y_trn_pred,Z_trn,W,V] = MLPTrain(X_trn_norm, y_trn, 3);
-%%%%
 
 % Predict Y for the validation and test set, using ForwardPropagation
-%%%% YOUR CODE STARTS HERE
 [Y_val_pred,Z_val] = ForwardPropagation(X_val_norm, W, V);
 [Y_tst_pred,Z_tst] = ForwardPropagation(X_tst_norm, W, V);
-%%%%
 
 % Do a 3D scatter showing Z for the training, validation and test set in
 % separate figures, using PlotZ3DScatter for each figure
-%%%% YOUR CODE STARTS HERE
 PlotZ3DScatter(Z_trn,Y_trn_pred)
 PlotZ3DScatter(Z_val,Y_val_pred)
 PlotZ3DScatter(Z_tst,Y_tst_pred)
-%%%% 
